44e61347e75b6a5cbde18b07e6583bba097fc2da
chanvirdi <email.chanpreetsingh@gmail.com>
2016-11-29 23:26:41 -0500

    master updated

--- hello.py
+++ hello.py
@@ -3,3 +3,5 @@ print "Welcome to Git\n";
 print "This is part of the hotfix branch\n";
 print "I am not sure what does branch means\n";
 print "New line after the hot fix is deleted\n";
+
+print "New line added to master after newBr branch\n";
