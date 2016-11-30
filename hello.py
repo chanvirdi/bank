9419908f2971871e446619a101511a697952e7b5
chanvirdi <email.chanpreetsingh@gmail.com>
2016-11-29 23:08:46 -0500

    Finish the commits

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 #!/usr/bin/perl
 print "Welcome to Git\n";
-print "I am not sure what does branch means";
+print "I am not sure what does branch means\n";
+print "New line after the hot fix is deleted\n";
